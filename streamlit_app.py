5675aac137349911feb1f577976f3166629f1cf6
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-10-13 22:53:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Chinese Translator", page_icon="📖")
+st.set_page_config(page_title="Translator", page_icon="📖")
 
 english = st.Page("english.py", title="Translate to English", icon="🦅")
 chinese = st.Page("chinese.py", title="Translate to Chinese", icon="🏮")
