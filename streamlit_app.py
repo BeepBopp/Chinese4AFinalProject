f708df2ded1c083278f729c7c67aadf7c9c09b42
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-10-13 22:55:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Translator", page_icon="📖")
 
 english = st.Page("english.py", title="Translate to English", icon="🦅")
 chinese = st.Page("chinese.py", title="Translate to Chinese", icon="🏮")
-culture = st.Page("culture.py", title="Writing Tips", icon="🌍")
+culture = st.Page("culture.py", title="Culture", icon="🌍")
 
 # Create navigation
 pg = st.navigation([english, chinese, culture])
