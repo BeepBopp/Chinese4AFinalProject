04f04d74d96e805852ed78e317a18012e80a9a0d
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-10-28 09:35:36 -0400

    Update culture.py

--- culture.py
+++ culture.py
@@ -1,4 +1,25 @@
 import streamlit as st
 
 st.title("Culture 🌍")
-st.markdown("Coming soon!")
+st.markdown("""
+### Childhood 小的时候
+United States 美国
+- Playing with friends 跟朋友玩儿
+- Monopoly 大富翁
+- American Football 美式足球
+China 中国
+- Mostly studying 学习
+- Chinese chess 波子棋
+- Shuttlecock 毽子
+### Food 餐
+United States 美国
+- Hamburgers 汉堡包
+- Chips / French Fries 薯片 / 薯条
+- Cookies 饼干
+China 中国
+- Dumplings 饺子
+- Fried rice / chow mein 炒饭 / 炒面
+- Mooncakes 月饼
+
+More topics + entries coming soon！
+""")
