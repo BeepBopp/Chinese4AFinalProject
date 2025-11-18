2336940083a48bea6136ae9829c9360f69918abe
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-11-18 10:37:18 -0500

    Update culture.py

--- culture.py
+++ culture.py
@@ -27,32 +27,32 @@ with st.expander("English Version"):
 
 with st.expander("Chinese Version"):
   st.markdown("""
-  A: 你好！我是美国人。 \
-  B: 你好！我是中国人。你有时间的时候喜欢做什么？ \
-  A: 我喜欢跟朋友踢球，还喜欢玩大富翁。你有时间的时候喜欢做什么？ \
-  B: 我喜欢玩毽子，可是我很多时候要学习。 \
-  A: 哦。我也要上学，可是我现在不怎么学习。 \
-  B: 你喜欢吃什么？我喜欢中餐，可是我也喜欢吃薯条。 \
-  A: 我也喜欢吃薯条！我也喜欢吃薯片！我很喜欢吃零食！你喜欢什么甜点？ \
-  B: 我很喜欢月饼。 \
-  A: 我也很喜欢月饼！月饼很甜，也很好吃。 \
-  B: 你觉得自己是个挑食的人吗？ \
-  A: 有时候。我不喜欢吃很多菜。 \
-  B: 我也不喜欢吃菜，可是有一些菜我喜欢吃。 \
-  A: 你喜欢去购物吗？ \
-  B: 我不喜欢去购物。我只跟我妈妈去购物。 \
-  A: 我姐姐也很喜欢购物，所以我经常跟她去购物。 \
-  B: 哦。可是，我很喜欢打折的东西。 \
+  A: 你好！我是美国人。 \\
+  B: 你好！我是中国人。你有时间的时候喜欢做什么？ \\
+  A: 我喜欢跟朋友踢球，还喜欢玩大富翁。你有时间的时候喜欢做什么？  \\
+  B: 我喜欢玩毽子，可是我很多时候要学习。  \\
+  A: 哦。我也要上学，可是我现在不怎么学习。  \\
+  B: 你喜欢吃什么？我喜欢中餐，可是我也喜欢吃薯条。 \\
+  A: 我也喜欢吃薯条！我也喜欢吃薯片！我很喜欢吃零食！你喜欢什么甜点？  \\
+  B: 我很喜欢月饼。 \\
+  A: 我也很喜欢月饼！月饼很甜，也很好吃。  \\
+  B: 你觉得自己是个挑食的人吗？ \\
+  A: 有时候。我不喜欢吃很多菜。 \\
+  B: 我也不喜欢吃菜，可是有一些菜我喜欢吃。  \\
+  A: 你喜欢去购物吗？ \\
+  B: 我不喜欢去购物。我只跟我妈妈去购物。  \\
+  A: 我姐姐也很喜欢购物，所以我经常跟她去购物。  \\
+  B: 哦。可是，我很喜欢打折的东西。 \\
   A: 我也很喜欢打折的东西！便宜的东西都很好。 
   """)
 
 with st.expander("Chinese Pinyin Version"):
   st.markdown("""
-  A: nǐ hǎo! wǒ shì měiguó rén. \
-  B: nǐ hǎo! Wǒ shì zhōngguó rén. nǐ yǒu shíjiān de shíhou xǐhuān zuò shénme? \
+  A: nǐ hǎo! wǒ shì měiguó rén.  \\
+  B: nǐ hǎo! Wǒ shì zhōngguó rén. nǐ yǒu shíjiān de shíhou xǐhuān zuò shénme?  \\
   A: wǒ xǐhuān gēn péngyǒu tīqiú, hái xǐhuān wán dàfùwēng. nǐ yǒu shíjiān de shíhou xǐhuān zuò shénme? \
-  B: wǒ xǐhuān wán jiànzi, kěshì wǒ hěn duō shíhou yào xuéxí. \
-  A: Ó. Wǒ yě yào shàngxué, kěshì wǒ xiànzài bù zěnme xuéxí. \
+  B: wǒ xǐhuān wán jiànzi, kěshì wǒ hěn duō shíhou yào xuéxí. \\
+  A: Ó. Wǒ yě yào shàngxué, kěshì wǒ xiànzài bù zěnme xuéxí.  \\
   B: nǐ xǐhuān chī shénme? wǒ xǐhuān Zhōngcān, kěshì wǒ yě xǐhuān chī shǔtiáo. \
   A: wǒ yě xǐhuān chī shǔtiáo! wǒ yě xǐhuān chī shǔpiàn! Wǒ hěn xǐhuān chī língshí! nǐ xǐhuān shénme tiándiǎn? \
   B: wǒ hěn xǐhuān yuèbǐng. \
