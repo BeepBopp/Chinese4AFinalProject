fe793cc06d9ce79876e42dbfa577fd16e46d4de5
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-11-18 09:42:53 -0500

    Update culture.py

--- culture.py
+++ culture.py
@@ -1,38 +1,68 @@
 import streamlit as st
 
 st.title("Culture 🌍")
-st.markdown("""
-### Childhood 小的时候
-United States 美国
-- Playing with friends 跟朋友玩儿
-- Monopoly 大富翁
-- American Football 美式足球
+st.header("Witness a dialogue between American and Chinese kids!")
 
-China 中国
-- Mostly studying 学习
-- Chinese chess 波子棋
-- Shuttlecock 毽子
+with st.expander("English Version"):
+  st.markdown("""
+  A: Hi! I am American!
+  B: Hi! I am Chinese! What do you like to do in your free time?
+  A: I like playing soccer and Monopoly with my friends! What do you do in your free time?
+  B: I like playing with my shuttlecock, but unfortunately, I have to study a lot.
+  A: Oh. I have school too, but I don't study that much.
+  B: What do you like to eat? I like to eat Chinese food, but I also like eating French fries.
+  A: I also like eating French fries! I also like potato chips! I love eating snacks! What is your favorite dessert？
+  B: My favorite desert are mooncakes!
+  A: I also like mooncakes! They are very sweet and yummy.
+  B: Do you think you are a picky eater?
+  A: Sometimes. I don't like eating a lot of vegetables.
+  B: I also don't like vegetables, but there a few vegetables I like.
+  A: Do you like to go shopping?
+  B: I don't like going shopping. I only go shopping with my mom.
+  A: My older sister likes going shopping, so I also only go with her.
+  B: I like to spot discounts though!
+  A: Me too! I love it when things are cheap!
+  
+  """)
 
-### Food 餐
-United States 美国
-- Hamburgers 汉堡包
-- Chips / French Fries 薯片 / 薯条
-- Cookies 饼干 / cake 蛋糕
+with st.expander("Chinese Version"):
+  st.markdown("""
+  A: 你好！我是美国人。
+  B: 你好！我是中国人。你有时间的时候喜欢做什么？
+  A: 我喜欢跟朋友踢球，还喜欢玩大富翁。你有时间的时候喜欢做什么？
+  B: 我喜欢玩毽子，可是我很多时候要学习。
+  A: 哦。我也要上学，可是我现在不怎么学习。
+  B: 你喜欢吃什么？我喜欢中餐，可是我也喜欢吃薯条。
+  A: 我也喜欢吃薯条！我也喜欢吃薯片！我很喜欢吃零食！你喜欢什么甜点？
+  B: 我很喜欢月饼。
+  A: 我也很喜欢月饼！月饼很甜，也很好吃。
+  B: 你觉得自己是个挑食的人吗？
+  A: 有时候。我不喜欢吃很多菜。
+  B: 我也不喜欢吃菜，可是有一些菜我喜欢吃。
+  A: 你喜欢去购物吗？
+  B: 我不喜欢去购物。我只跟我妈妈去购物。
+  A: 我姐姐也很喜欢购物，所以我经常跟她去购物。
+  B: 哦。可是，我很喜欢打折的东西。
+  A: 我也很喜欢打折的东西！便宜的东西都很好。 
+  """)
 
-China 中国
-- Dumplings 饺子
-- Fried rice / chow mein 炒饭 / 炒面
-- Mooncakes 月饼 / tang yuan 汤圆
-
-### Shopping 购物
-United States 美国
-- Online shopping 网店
-- Flagship stores 旗舰店
-- Discounts 打折
-
-China 中国
-- Online shopping 网店
-- Specialty stores 专卖店
-- Adding eggs (and other practical things) 加鸡蛋 
-More topics + entries coming soon！
-""")
+with st.expander("Chinese Pinyin Version"):
+  st.markdown("""
+  A: nǐ hǎo! wǒ shì měiguó rén.
+  B: nǐ hǎo! Wǒ shì zhōngguó rén. nǐ yǒu shíjiān de shíhou xǐhuān zuò shénme?
+  A: wǒ xǐhuān gēn péngyǒu tīqiú, hái xǐhuān wán dàfùwēng. nǐ yǒu shíjiān de shíhou xǐhuān zuò shénme?
+  B: wǒ xǐhuān wán jiànzi, kěshì wǒ hěn duō shíhou yào xuéxí.
+  A: Ó. Wǒ yě yào shàngxué, kěshì wǒ xiànzài bù zěnme xuéxí.
+  B: nǐ xǐhuān chī shénme? wǒ xǐhuān Zhōngcān, kěshì wǒ yě xǐhuān chī shǔtiáo.
+  A: wǒ yě xǐhuān chī shǔtiáo! wǒ yě xǐhuān chī shǔpiàn! Wǒ hěn xǐhuān chī língshí! nǐ xǐhuān shénme tiándiǎn?
+  B: wǒ hěn xǐhuān yuèbǐng.
+  A: wǒ yě hěn xǐhuān yuèbǐng! yuèbǐng hěn tián, yě hěn hǎochī.
+  B: nǐ juéde zìjǐ shì gè tiāoshí de rén ma?
+  A: yǒu shíhou. wǒ bù xǐhuān chī hěn duō cài.
+  B: wǒ yě bù xǐhuān chī cài, kěshì yǒu yīxiē cài wǒ xǐhuān chī.
+  A: nǐ xǐhuān qù gòuwù ma?
+  B: wǒ bù xǐhuān qù gòuwù. wǒ zhǐ gēn wǒ māma qù gòuwù.
+  A: wǒ jiějie yě hěn xǐhuān gòuwù, suǒyǐ wǒ jīngcháng gēn tā qù gòuwù.
+  B: Ó. kěshì, wǒ hěn xǐhuān dǎzhé de dōngxi.
+  A: wǒ yě hěn xǐhuān dǎzhé de dōngxi! piányi de dōngxi dōu hěn hǎo.
+  """)
